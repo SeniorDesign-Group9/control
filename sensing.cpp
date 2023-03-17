a6ffb5007d62a0fa0a231acae14f393d457d206b
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-03-17 14:20:42 -0400

    removing mc from sensing class

--- sensing.cpp
+++ sensing.cpp
@@ -17,52 +17,5 @@ Sensing& Sensing::instance() {
     return inst;
 }
 
-// Step motor forward 1 step
-void DRV8833::stepForwardOne(void) {
-    stepSteps(static_cast<int32_t>(1));
-    return;
-}
-
-// Step motor backward 1 step
-void DRV8833::stepBackwardOne(void) {
-    stepSteps(static_cast<int32_t>(-1));
-    return;
-}
-
-// Step motor to position pos
-void DRV8833::stepPosition(uint32_t desired_pos) {
-    int32_t steps = 0;
-
-    // FIXME: Lock current_pos
-    steps = desired_pos - current_pos;
-    // FIXME: Unlock current_pos
-    stepSteps(steps);
-
-    return;
-}
-
-// Step motor to position 0
-void DRV8833::stepZero(void) {
-    int32_t steps = 0;
-
-    // FIXME: Lock current_pos
-    steps = -current_pos;
-    // FIXME: Unlock current_pos
-    stepSteps(steps);
-
-    return;
-}
-
-// Step motor to position max_steps
-void DRV8833::stepMax(void) {
-    int32_t steps = 0;
-
-    // FIXME: Lock current_pos
-    steps = MAX_STEP - current_pos;
-    // FIXME: Unlock current_pos
-    stepSteps(steps);
-
-    return;
-}
 
 
