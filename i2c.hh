50ce309cca1d7ede179f192bada2fb2c733ddde1
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-04-03 22:25:50 -0400

    i2c abstraction

--- i2c.hh
+++ i2c.hh
@@ -4,14 +4,30 @@
 
 #ifndef CONTROL_I2C_H
 #define CONTROL_I2C_H
-typedef struct register {
-   uint8_t reg
-}Register;
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <ti/drivers/I2C.h>
 class I2C {
 public:
-    bool init(I2C_Handle i2cHandle, uint8_t i2cAddress);
+    // Init ADC
+    bool init(I2C_Handle i2cHandle, uint8_t device_address);
+
+    // Performs I2C transfer with txBuffer, rxBuffer, txCount, and rxCount
+    // Returns true on transfer success, false on failure
     bool transfer();
-    bool setRawRegisterValue(Register reg, uint8_t data);
-    bool
+
+    // I2C error handler
+    void i2cErrorHandler(I2C_Transaction *transaction);
+
+protected:
+    I2C_Handle i2cHandle;
+    I2C_Transaction i2cTransaction;
+    uint8_t device_address;
+    uint8_t txBuffer[3];
+    uint8_t txCount;
+    uint8_t rxBuffer[2];
+    uint8_t rxCount;
 };
+
 #endif //CONTROL_I2C_H
