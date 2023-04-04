e858a23783d523560dd4cbb7f3118c17331b8d65
Nicholas Chitty <nicholas.chitty@gmail.com>
2023-04-03 22:26:57 -0400

    i honestly cant remember

--- charger.cpp
+++ charger.cpp
@@ -2,4 +2,37 @@
 // Created by nicho on 4/3/2023.
 //
 
-#include "charger.h"
+#include "charger.hh"
+
+Charger::Charger() {}
+
+Charger::~Charger() {}
+
+Charger& Charger::instance() {
+    static Charger inst;
+    return inst;
+}
+
+bool Charger::setRawRegisterValue(uint8_t reg, uint8_t lsb, uint8_t msb) {
+    txBuffer[0] = reg;
+    txBuffer[1] = lsb;
+    txBuffer[2] = msb;
+
+    txCount = 3;
+    rxCount = 0;
+
+    return transfer();
+}
+
+uint16_t Charger::getRawRegisterValue(uint8_t reg) {
+    txBuffer[0] = reg;
+
+    txCount = 1;
+    rxCount = 2;
+
+    if(!transfer()) {
+        return 0;
+    } else {
+        return (rxBuffer[1] << 8) + rxBuffer[0];
+    }
+}
